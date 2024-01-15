af7502408c6babf96022224e0bbf4cdf536ff44d
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-01-15 13:02:38 +0530

    adding app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Assignments from './components/assignment/Assignments';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Assignments />
     </div>
   );
 }
